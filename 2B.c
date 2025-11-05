5633fb215b76617a76782e38ccbf1833a8c0f783
Peng Tianxiang <932328811@qq.com>
2025-11-05 14:19:01 +0800

    20251105 version

--- 2B.c
+++ 2B.c
@@ -6,8 +6,8 @@
 void transpose(int *matrix, int m, int n, int ld) {
     for (int i = 0; i < ld; i++) { // Loop over the rows
         for (int j = i + 1; j < ld; j++) { // Loop over the columns, j starts from i+1,the upper triangular part
-            int temp = matrix[i * ld + j]; // swap the corresponding elements,store the upper triangular part
-            matrix[i * ld + j] = matrix[j * ld + i];
+            int temp = matrix[i * ld + j]; // swap the corresponding elements,store the upper triangular part,(i,j)to(j,i)
+            matrix[i * ld + j] = matrix[j * ld + i];//
             matrix[j * ld + i] = temp;
         }
     }
